fbeab6ed70bfc6a9c3447f98316f65238de0e948
fukyor <3273228775@qq.com>
2026-04-21 12:51:26 +0800

    tmp

--- src/views/loginView.vue
+++ src/views/loginView.vue
@@ -3,7 +3,7 @@ import { ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import { useWebSocketStore } from '@/stores/websocket'
 
-const apiUrl = ref('http://127.0.0.1:8000')
+const apiUrl = ref(window.location.origin)
 const secret = ref('123')
 const label = ref('')
 const servers = ref([])
